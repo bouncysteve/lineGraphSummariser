13177120916d72522e216a9bb79719a63ddfc6bb
Steve <gitsteve@megatno.com>
2016-06-23 19:48:36 +0100

    Make nlgFactory non-static in test for test isolation

--- src/test/java/uk/co/lgs/text/service/segment/graph/SegmentDurationDescriberServiceImplTest.java
+++ src/test/java/uk/co/lgs/text/service/segment/graph/SegmentDurationDescriberServiceImplTest.java
@@ -2,6 +2,7 @@ package uk.co.lgs.text.service.segment.graph;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 
@@ -16,14 +17,16 @@ import uk.co.lgs.test.AbstractTest;
 public class SegmentDurationDescriberServiceImplTest extends AbstractTest {
 
     private static final Lexicon lexicon = Lexicon.getDefaultLexicon();
-    private static final NLGFactory NLG_FACTORY = new NLGFactory(lexicon);
     private static final Realiser realiser = new Realiser(lexicon);
 
     private static final String START_TIME = "ages ago";
-    private static final NPPhraseSpec START_TIME_PHRASE = NLG_FACTORY.createNounPhrase(START_TIME);
-
     private static final String END_TIME = "just now";
-    private static final NPPhraseSpec END_TIME_PHRASE = NLG_FACTORY.createNounPhrase(END_TIME);
+
+    private NLGFactory nlgFactory;
+
+    private NPPhraseSpec startTimePhrase;
+
+    private NPPhraseSpec endTimePhrase;
 
     @Mock
     GraphSegment graphSegment;
@@ -33,25 +36,39 @@ public class SegmentDurationDescriberServiceImplTest extends AbstractTest {
     private SPhraseSpec summary;
     private String summaryText;
 
+    @Before
+    public void beforeEachTest() {
+        this.nlgFactory = new NLGFactory(lexicon);
+        this.startTimePhrase = this.nlgFactory.createNounPhrase(START_TIME);
+        this.endTimePhrase = this.nlgFactory.createNounPhrase(END_TIME);
+    }
+
     @Test
     public void testGenerateBetweenPhrase() {
-        this.summary = this.underTest.generateBetweenPhrase(START_TIME_PHRASE, END_TIME_PHRASE);
+        this.summary = this.underTest.generateBetweenPhrase(this.startTimePhrase, this.endTimePhrase);
         this.summaryText = realiser.realise(this.summary).toString();
         assertEquals("between " + START_TIME + " and " + END_TIME, this.summaryText);
     }
 
     @Test
     public void testGeneratePeriodPhrase() {
-        this.summary = this.underTest.generatePeriodPhrase(START_TIME_PHRASE, END_TIME_PHRASE);
+        this.summary = this.underTest.generatePeriodPhrase(this.startTimePhrase, this.endTimePhrase);
         this.summaryText = realiser.realise(this.summary).toString();
         assertEquals("during the period " + START_TIME + " to " + END_TIME, this.summaryText);
     }
 
     @Test
     public void testFromUntilPhrase() {
-        this.summary = this.underTest.generateFromUntilPhrase(START_TIME_PHRASE, END_TIME_PHRASE);
+        this.summary = this.underTest.generateFromUntilPhrase(this.startTimePhrase, this.endTimePhrase);
         this.summaryText = realiser.realise(this.summary).toString();
         assertEquals("from " + START_TIME + " until " + END_TIME, this.summaryText);
+    }
+
+    @Test
+    public void testUptoPhrase() {
+        this.summary = this.underTest.generateUpToPhrase(this.endTimePhrase);
+        this.summaryText = realiser.realise(this.summary).toString();
+        assertEquals("up to " + END_TIME, this.summaryText);
     }
 
 }
