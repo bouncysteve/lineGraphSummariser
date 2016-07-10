e97e4e64ba9e7d8ef74a5bffaadcddc90f21160f
Steve <gitsteve@megatno.com>
2016-07-10 22:39:34 +0100

    Revert GraphModel injection changes, as they didn't work (kept isIntersecting() method)

--- src/main/java/uk/co/lgs/Controller.java
+++ src/main/java/uk/co/lgs/Controller.java
@@ -71,8 +71,7 @@ public class Controller {
             LOG.info(graphData.toString());
 
             // *************Model ***************************//
-            final GraphModel model = new GraphModelImpl();
-            model.setGraphData(graphData);
+            final GraphModel model = new GraphModelImpl(graphData);
             LOG.info(model.toString());
             final GraphSummaryService graphSummariser = context.getBean(GraphSummaryService.class);
             final String summary = graphSummariser.getSummary(model);
